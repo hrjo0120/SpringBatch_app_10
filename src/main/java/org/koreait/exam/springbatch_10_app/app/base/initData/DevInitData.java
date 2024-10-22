4b5bc2436daa55e23eecd326e931bea75e23b949
hrjo0120 <hrjo0120@gmail.com>
2024-10-22 16:10:42 +0900

    initData 기반, JPA 기반, AppConfig 기반

---
+++ src/main/java/org/koreait/exam/springbatch_10_app/app/base/initData/DevInitData.java
@@ -0,0 +1,17 @@
+package org.koreait.exam.springbatch_10_app.app.base.initData;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("dev")
+public class DevInitData implements InitDataBefore {
+    @Bean
+    CommandLineRunner initData() {
+        return args -> {
+            before();
+        };
+    }
+}
---
+++ src/main/java/org/koreait/exam/springbatch_10_app/app/base/initData/InitDataBefore.java
@@ -0,0 +1,7 @@
+package org.koreait.exam.springbatch_10_app.app.base.initData;
+
+public interface InitDataBefore {
+    default void before() {
+
+    }
+}
