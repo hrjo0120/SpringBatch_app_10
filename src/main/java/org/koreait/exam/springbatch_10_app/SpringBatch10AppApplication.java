bede631aab8edd9ce06e56148a1902b0290c7167
hrjo0120 <hrjo0120@gmail.com>
2024-10-22 15:12:44 +0900

    프로젝트 세팅

---
+++ src/main/java/org/koreait/exam/springbatch_10_app/SpringBatch10AppApplication.java
@@ -0,0 +1,13 @@
+package org.koreait.exam.springbatch_10_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatch10AppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatch10AppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/koreait/exam/springbatch_10_app/SpringBatch10AppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.koreait.exam.springbatch_10_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatch10AppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
