c4e421e3890c3abd159ed8d20e237f87c1fa45bf
hrjo0120 <hrjo0120@gmail.com>
2024-10-25 15:17:09 +0900

    정산 데이터 생성

--- src/main/java/com/koreait/exam/springbatch_10_app/app/order/repository/OrderItemRepository.java
+++ src/main/java/com/koreait/exam/springbatch_10_app/app/order/repository/OrderItemRepository.java
@@ -1,7 +1,11 @@
 package com.koreait.exam.springbatch_10_app.app.order.repository;
 
-import com.koreait.exam.springbatch_10_app.app.order.entity.Order;
+import com.koreait.exam.springbatch_10_app.app.order.entity.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrderItemRepository  extends JpaRepository<Order, Long> {
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+    List<OrderItem> findAllByPayDateBetween(LocalDateTime fromDate, LocalDateTime toDate);
 }
